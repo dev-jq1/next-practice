bb2d6b67e96403b2d11067730db2e6d64607c505
dev-jq1 <dev.jq1@gmail.com>
2023-04-15 23:54:36 +0900

    화면 구성 수정

--- app/list/page.js
+++ app/list/page.js
@@ -1,8 +1,16 @@
 export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
   return (
-    <main>
-      <h1 className="title">Programming Log</h1>
-      <p className="title-sub">by dev kim</p>
-    </main>
+    <div>
+      <h4 className="title">상품목록</h4>
+      {상품.map((el, idx) => {
+        return (
+          <div className="food" key={idx}>
+            <img src={`/food${idx}.png`} className="food-img" />
+            <h4>{el} $40</h4>
+          </div>
+        );
+      })}
+    </div>
   );
 }
