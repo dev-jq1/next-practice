a2ca6a5fad37fa15536d6a349b64b98da71b6fb6
dev-jq1 <dev.jq1@gmail.com>
2023-04-16 23:43:51 +0900

    client component 변경 후 useState 사용

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
 export default function List() {
-  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+  const items = ["Tomatoes", "Pasta", "Coconut"];
+  const [qty, setQty] = useState(0);
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {상품.map((el, idx) => {
+      {items.map((el, idx) => {
         return (
           <div className="food" key={idx}>
             <img src={`/food${idx}.png`} className="food-img" />
             <h4>{el} $40</h4>
+            <span>{qty}</span>
+            <button
+              type="button"
+              onClick={() => {
+                setQty(qty + 1);
+              }}
+            >
+              +
+            </button>
           </div>
         );
       })}
