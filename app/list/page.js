e2c11620d2d8336719ba424b8fc4ead5772c4dd5
dev-jq1 <dev.jq1@gmail.com>
2023-04-17 21:46:27 +0900

    수량 개별 증가

--- app/list/page.js
+++ app/list/page.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function List() {
   const items = ["Tomatoes", "Pasta", "Coconut"];
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(Array(items.length).fill(0));
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -13,11 +13,13 @@ export default function List() {
           <div className="food" key={idx}>
             <img src={`/food${idx}.png`} className="food-img" />
             <h4>{el} $40</h4>
-            <span>{qty}</span>
+            <span>{qty[idx]}</span>
             <button
               type="button"
               onClick={() => {
-                setQty(qty + 1);
+                const copy = [...qty];
+                copy[idx] = copy[idx] + 1;
+                setQty([...copy]);
               }}
             >
               +
